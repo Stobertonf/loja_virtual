dbc7b5df1a33eb0064da86359876e2562f9a6789
stobertonf <stobertonf@gmail.com>
2025-03-04 15:06:31 -0300

    feat(Producs-Tab): "Creating The Product Tile File."

---
+++ lib/tiles/product_tile.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class ProductTile extends StatelessWidget {
+  const ProductTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
